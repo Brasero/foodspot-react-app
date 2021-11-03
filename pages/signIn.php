192f0470e3d94eb67e24aac042f52caafd4ea069
Brasero <brandonricci4@gmail.com>
2021-11-03 21:55:44 +0100

    finish inscription page & logic

--- pages/signIn.php
+++ pages/signIn.php
@@ -47,6 +47,10 @@ if(!isset($succMsg)){
         <input class="form-control" type="email" id="inputEmail" name="signIn_email" placeholder="Adresse Email" />
         <label for="inputEmail">Adresse Email</label>
     </div>
+    <div class="form-floating mb-3">
+       <input class="form-control" id="inputNum" type="number" name="signIn_num" placeholder="Téléphone" />
+       <label for="inputNum">Numéro de téléphone</lebel> 
+    </div>
     <div class="form-floating mb-2">
         <input class="form-control" type="password" id="inputPassword" name="signIn_password" auto-complete="current-password" placeholder="Mot de passe" />
         <label for="inputPassword">Mot de passe</label>
@@ -80,10 +84,69 @@ else if(isset(
     !empty($_POST['signIn_confirm_password'])){
 
         if($_POST['signIn_password'] === $_POST['signIn_confirm_password']){
-            
-            echo '<div class="text-success h3 text-center">'.$succMsg.' Toutes les verifications sont OK.</div>';
 
             //Here goes the db insert after the mail verification.
+
+            $dbUsersQuery = $bdd->connexion->query('SELECT * FROM users');
+            $dbUsersArray = $dbUsersQuery->fetchAll();
+            $emailExist = false;
+
+            foreach($dbUsersArray as $dbUsers){
+                if($dbUsers['mail_users'] === $_POST['signIn_email']){
+                    $emailExist = true;
+                    break;
+                }
+            }
+
+            if($emailExist){
+                echo'<div class="text-danger text-center h3">Votre adresse email est déjà connue de nos systéme, connectez-vous.</div>';
+            }
+            else{
+                $nom = $_POST['signIn_nom'];
+                $prenom = $_POST['signIn_prenom'];
+                $adresse = $_POST['signIn_adresse'];
+                $ville = $_POST['signIn_ville'];
+                $email = $_POST['signIn_email'];
+                $num = $_POST['signIn_num'];
+                $pass = $_POST['signIn_password'];
+                echo $ville;
+
+                $hashPass = password_hash($pass, PASSWORD_DEFAULT);
+
+                $villeQuery = $bdd->connexion->query('SELECT * FROM ville WHERE nom LIKE "'.$ville.'%"');
+                
+                if($villeQuery != false){
+                    $dbville = $villeQuery->fetch();
+                    $villeId = $dbville['ID'];
+                    $identifiant = time();
+
+                    $queryStr = 'INSERT INTO users (identifiant_users, nom_users, prenom_users, mail_users, adresse_users, id_ville_users, tel_users, mdp_users, isManager_users)
+                                VALUES (:identifiant, :nom, :prenom, :mail, :adresse, :idVille, :num, :mdp, 0)';
+                    $insertQuery = $bdd->connexion->prepare($queryStr);
+
+                    $insertQuery->bindValue(':identifiant', $identifiant, PDO::PARAM_INT);
+                    $insertQuery->bindValue(':nom', $nom, PDO::PARAM_STR);
+                    $insertQuery->bindValue(':prenom', $prenom, PDO::PARAM_STR);
+                    $insertQuery->bindValue(':mail', $email, PDO::PARAM_STR);
+                    $insertQuery->bindValue(':adresse', $adresse, PDO::PARAM_STR);
+                    $insertQuery->bindValue(':idVille', $villeId, PDO::PARAM_INT);
+                    $insertQuery->bindValue(':num', $num, PDO::PARAM_INT);
+                    $insertQuery->bindValue(':mdp', $hashPass, PDO::PARAM_STR);
+
+                    if($insertQuery->execute()){
+                        echo '<div class="text-success text-center h4">Votre inscription a bien été enregistrée.</div>';
+                    }
+                    else{
+                        echo '<div class="text-danger text-center h4">Une erreur est survenue.</br>Vérifiez votre connexion et recommencez.</div>';
+                        var_dump($insertQuery->errorInfo());
+                    }
+                }
+                else{
+                    echo '<div class="text-danger text-center h4">Nous ne connaissons pas votre ville désolé.</div>';
+                }
+                
+
+            }
 
         }
         else{
@@ -114,6 +177,9 @@ else if(isset(
                     <input class="form-control" type="email" id="inputEmail" name="signIn_email" placeholder="Adresse Email" />
                     <label for="inputEmail">Adresse Email</label>
                 </div>
+                <div class="form-floating mb-3">
+               <input class="form-control" id="inputNum" type="number" name="signIn_num" placeholder="Téléphone" />
+               <label for="inputNum">Numéro de téléphone</lebel> </div>
                 <div class="form-floating mb-2">
                     <input class="form-control" type="password" id="inputPassword" name="signIn_password" auto-complete="current-password" placeholder="Mot de passe" />
                     <label for="inputPassword">Mot de passe</label>
@@ -158,6 +224,9 @@ else if(!isset($succMsg)){
             <input class="form-control" type="email" id="inputEmail" name="signIn_email" placeholder="Adresse Email" />
             <label for="inputEmail">Adresse Email</label>
         </div>
+        <div class="form-floating mb-3">
+       <input class="form-control" id="inputNum" type="number" name="signIn_num" placeholder="Téléphone" />
+       <label for="inputNum">Numéro de téléphone</lebel> </div>
         <div class="form-floating mb-2">
             <input class="form-control" type="password" id="inputPassword" name="signIn_password" auto-complete="current-password" placeholder="Mot de passe" />
             <label for="inputPassword">Mot de passe</label>
@@ -203,6 +272,9 @@ else{
             <input class="form-control" type="email" id="inputEmail" name="signIn_email" placeholder="Adresse Email" />
             <label for="inputEmail">Adresse Email</label>
         </div>
+        <div class="form-floating mb-3">
+       <input class="form-control" id="inputNum" type="number" name="signIn_num" placeholder="Téléphone" />
+       <label for="inputNum">Numéro de téléphone</lebel> </div>
         <div class="form-floating mb-2">
             <input class="form-control" type="password" id="inputPassword" name="signIn_password" auto-complete="current-password" placeholder="Mot de passe" />
             <label for="inputPassword">Mot de passe</label>
